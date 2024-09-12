6e8f564d2d90d9dd844f28f922b7733b8c6272f0
my 1st repository <supriyadhal50@gmail.com>
2024-09-13 00:17:31 +0530

    mongodb database connection logic build

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+type connectionObject = {
+  isConnected?: number;
+};
+
+//the property "isConnected" is option , so the variable connection can be empty also
+const connection: connectionObject = {};
+
+async function connectDB(): Promise<void> {
+  //first check the database is connected or not
+  if (connection.isConnected) {
+    console.log("database has already connected");
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI || "", {});
+
+    connection.isConnected = db.connections[0].readyState;
+    console.log("DATABASE connected successfully");
+  } catch (error) {
+    console.log("DATABASE connection failed");
+    process.exit(1);
+  }
+}
+
+export default connectDB;
