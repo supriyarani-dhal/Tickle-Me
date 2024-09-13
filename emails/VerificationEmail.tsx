548e935348614f5e74e49ae78f6a57f52dd77bc3
my 1st repository <supriyadhal50@gmail.com>
2024-09-14 00:56:34 +0530

    created email API for verification, using 'Sender' api service and 'React-email' templates

---
+++ emails/VerificationEmail.tsx
@@ -0,0 +1,58 @@
+import {
+  Font,
+  Head,
+  Heading,
+  Html,
+  Preview,
+  Row,
+  Section,
+  Text,
+} from "@react-email/components";
+
+interface VerificationEmailProps {
+  userName: string;
+  otp: string;
+}
+
+export default function VerificationEmail({
+  userName,
+  otp,
+}: VerificationEmailProps) {
+  return (
+    <Html lang="en" dir="ltr">
+      <Head>
+        <title>Verification Code</title>
+        <Font
+          fontFamily="Robot"
+          fallbackFontFamily="Verdana"
+          webFont={{
+            url: "https://fonts.gstatic.com/s/roboto/v27/KFOmCnqEu92Fr1Mu4mxKKTU1Kg.woff2",
+            format: "woff",
+          }}
+          fontWeight={400}
+          fontStyle="normal"
+        />
+      </Head>
+      <Preview>Here&apos;s yourverification code: {otp}</Preview>
+      <Section>
+        <Row>
+          <Heading as="h2">Hello {userName},</Heading>
+        </Row>
+        <Row>
+          <Text>
+            Thank you for registering, please use the following verification
+            code to complete your registration:
+          </Text>
+        </Row>
+        <Row>
+          <Text>{otp}</Text>
+        </Row>
+        <Row>
+          <Text>
+            If you didn not request this code , please ignore this email.
+          </Text>
+        </Row>
+      </Section>
+    </Html>
+  );
+}
---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,22 @@
+import { resend } from "@/lib/resend";
+import VerificationEmail from "../../emails/VerificationEmail";
+import { ApiResponse } from "@/types/apiResponse";
+
+export async function sendVerificationEmail(
+  email: string,
+  userName: string,
+  verifyCode: string
+): Promise<ApiResponse> {
+  try {
+    await resend.emails.send({
+      from: "supriyadhal50@gmail.com",
+      to: email,
+      subject: "Tickle-Me verification code",
+      react: VerificationEmail({ userName, otp: verifyCode }),
+    });
+    return { success: true, message: "Verification email send successfully" };
+  } catch (emailError) {
+    console.error("error sending verification email", emailError);
+    return { success: false, message: "Failed to send verification email" };
+  }
+}
